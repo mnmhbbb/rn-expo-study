df7785153ce5ecb8f45cf6fad9a8f84feb53857e
mnmhbbb <mnmhb64@gmail.com>
2025-08-14 00:09:59 +0900

    feat: axios 인터셉트 생성

---
+++ api/axios.ts
@@ -0,0 +1,14 @@
+import axios from "axios";
+import { Platform } from "react-native";
+
+const baseUrls = {
+  android: "http://10.0.2.2:3030",
+  ios: "http://localhost:3030",
+  //   ios: "http://192.168.219.143:3030",
+};
+
+const axiosInstance = axios.create({
+  baseURL: baseUrls[Platform.OS as keyof typeof baseUrls],
+});
+
+export default axiosInstance;
