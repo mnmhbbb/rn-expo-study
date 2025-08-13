8acb32a4307815ab6b45f1b8cf201217e9027145
mnmhbbb <mnmhb64@gmail.com>
2025-08-14 00:19:38 +0900

    feat: useAuth 훅 생성

---
+++ hooks/queries/useAuth.ts
@@ -0,0 +1,61 @@
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { router } from "expo-router";
+import { useEffect } from "react";
+
+import { getMe, postLogin, postSignup } from "@/api/auth";
+import queryClient from "@/api/queryClient";
+import { removeHeader, setHeader } from "@/utils/header";
+import { deleteSecureStore, setSecureStore } from "@/utils/secureStore";
+
+const useGetMe = () => {
+  const { data, isError } = useQuery({
+    queryFn: getMe,
+    queryKey: ["auth", "getMe"],
+  });
+
+  useEffect(() => {
+    if (isError) {
+      removeHeader("Authorization");
+      deleteSecureStore("accessToken");
+    }
+  }, [isError]);
+
+  return { data };
+};
+
+const useLogin = () => {
+  return useMutation({
+    mutationFn: postLogin,
+    onSuccess: async ({ accessToken }) => {
+      setHeader("Authorization", `Bearer ${accessToken}`);
+      await setSecureStore("accessToken", accessToken);
+      queryClient.fetchQuery({ queryKey: ["auth", "getMe"] });
+      router.replace("/");
+    },
+    onError: () => {},
+  });
+};
+
+const useSignup = () => {
+  return useMutation({
+    mutationFn: postSignup,
+    onSuccess: () => router.replace("/auth/login"),
+    onError: () => {},
+  });
+};
+
+const useAuth = () => {
+  const { data } = useGetMe(); // useAuth 훅을 사용하는 컴포넌트에서는 항상 실행 됨
+  const loginMutation = useLogin();
+  const signupMutation = useSignup();
+
+  return {
+    auth: {
+      id: data?.id || "",
+    },
+    loginMutation,
+    signupMutation,
+  };
+};
+
+export default useAuth;
