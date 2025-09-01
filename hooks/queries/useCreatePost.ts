4518854e6e711d1fce13eba055dbe2d6f62555c8
mnmhbbb <mnmhb64@gmail.com>
2025-09-02 00:18:42 +0900

    feat: 게시글 작성 성공 시 쿼리 무효화 기능 추가

--- hooks/queries/useCreatePost.ts
+++ hooks/queries/useCreatePost.ts
@@ -1,12 +1,16 @@
-import { createPost } from "@/api/post";
 import { useMutation } from "@tanstack/react-query";
 import { router } from "expo-router";
+
+import { createPost } from "@/api/post";
+import queryClient from "@/api/queryClient";
+import { queryKeys } from "@/constants";
 
 const useCreatePost = () => {
   return useMutation({
     mutationFn: createPost,
     onSuccess: () => {
       router.replace("/");
+      queryClient.invalidateQueries({ queryKey: [queryKeys.POSTS, queryKeys.GET_POSTS] }); // 쿼리무효화
     },
   });
 };
