a43e27cf31bd9ccabd108e5edffa877482bd8f0b
mnmhbbb <mnmhb64@gmail.com>
2025-09-04 00:23:08 +0900

    feat: @expo/react-native-action-sheet 패키지 추가 및 ActionSheetProvider로 루트 레이아웃 감싸기

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,3 +1,4 @@
+import { ActionSheetProvider } from "@expo/react-native-action-sheet";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
@@ -27,10 +28,12 @@ export default function RootLayout() {
   }
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <RootNavigator />
-      <Toast />
-    </QueryClientProvider>
+    <ActionSheetProvider>
+      <QueryClientProvider client={queryClient}>
+        <RootNavigator />
+        <Toast />
+      </QueryClientProvider>
+    </ActionSheetProvider>
   );
 }
 
